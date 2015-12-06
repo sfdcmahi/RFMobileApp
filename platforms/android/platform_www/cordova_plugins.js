8bfad42881400bce75522587098f84262cced19a
sfdcmahi@gmail.com <sfdcmahi@gmail.com>
2015-12-06 13:57:48 +0530

    Basic Ionic files

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,27 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "pluginId": "com.phonegap.plugins.PushPlugin",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.0",
+    "com.phonegap.plugins.PushPlugin": "2.2.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ www/js/config.js
@@ -0,0 +1,11 @@
+angular.module('config', [])
+
+  .constant('forcengOptions', {
+	proxyURL: 'https://rfmobileapp.herokuapp.com/'
+  })
+
+  // baseURL should be left to empty string. This value is only used when you want to use the same app in a Visualforce
+  // page where you have to account for the path to the static resource. In that case the config module is created from
+  // within index.vf where the path to the static resource can be obtained.
+  .constant('baseURL', '');
+
